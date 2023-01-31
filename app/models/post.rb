27726f7350d99c76686cc0ae66771ad5659ff6f8
Rohan James <rohanjames9282@gmail.com>
2023-01-31 12:47:09 +0530

    Add post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :content { minimum: 5 }, presence: true
-  validates :title { minimum: 1 }, presence: true
-  validates :user, presence: true
-
+  validates :content, length: { minimum: 5 }, presence: true
+  validates :title, length: { minimum: 1 }, presence: true
+  validates_associated :user
 end
